1e225d9d97d17037affe97a37631a01dd0dacd3a
yanxugithub <yan_xu_nj@hotmail.com>
2024-06-08 22:06:13 -0400

    initial practices
    initial practices

---
+++ refreturn/refreturn/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using refreturn;
+
+Class1 c1 = new Class1();
+
+Class2 c2 = new();
+
+Console.WriteLine("Hello, World!");
+
+int x = 124;
+
+ref int foo(ref int input)
+{
+    int ret = input;
+    return ref input;
+}
+
+Console.WriteLine($"x={x}");
+
+ref int y = ref foo(ref x);
+
+Console.WriteLine($"y={y}");
+
+y = 33;
+
+Console.WriteLine($"y={y}");
+
+Console.WriteLine($"x={x}");
+
+
+
